fe7a9f8898446eed6fb90ebaf753d9fbee606038
Ben Nizette <benjamin.nizette@anu.edu.au>
2014-02-20 16:58:41 +1100

    Show: Turn on messages to show

--- ublox_show.py
+++ ublox_show.py
@@ -7,11 +7,22 @@ from optparse import OptionParser
 parser = OptionParser("ublox_show.py [options] <file>")
 parser.add_option("--types", default='*', help="comma separated list of types to show (wildcards allowed)")
 parser.add_option("--seek", type='float', default=0, help="seek percentage to start in log")
+parser.add_option("--baudrate", type='int',
+                  help="serial baud rate", default=115200)
 parser.add_option("-f", "--follow", action='store_true', default=False, help="ignore EOF")
 
 (opts, args) = parser.parse_args()
 
-dev = ublox.UBlox(args[0])
+dev = ublox.UBlox(args[0], baudrate=opts.baudrate)
+
+dev.configure_message_rate(ublox.CLASS_NAV, ublox.MSG_NAV_POSLLH, 1)
+dev.configure_message_rate(ublox.CLASS_NAV, ublox.MSG_NAV_POSECEF, 1)
+dev.configure_message_rate(ublox.CLASS_NAV, ublox.MSG_NAV_SOL, 1)
+dev.configure_message_rate(ublox.CLASS_RXM, ublox.MSG_RXM_RAW, 1)
+dev.configure_message_rate(ublox.CLASS_RXM, ublox.MSG_RXM_SFRB, 1)
+dev.configure_message_rate(ublox.CLASS_AID, ublox.MSG_AID_EPH, 1)
+dev.configure_message_rate(ublox.CLASS_NAV, ublox.MSG_NAV_SVINFO, 1)
+dev.configure_solution_rate(rate_ms=200)
 
 if opts.seek != 0:
     dev.seek_percent(opts.seek)
